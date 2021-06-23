062e353a7eb28987f9dee73f6dd592067e768b2a
Gergely Csecsey <gcsecsey@gmail.com>
2021-06-23 20:03:33 +0000

    exercise d solution

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -19,7 +19,11 @@ fn main() {
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
-
+        match arg.as_str() {
+            "sum" => sum(),
+            "double" => double(),
+            _ => count(arg),
+        }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -31,8 +35,9 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-
-
+    for n in 7..=23 {
+        sum += n;
+    }
     println!("The sum is {}", sum);
 }
 
@@ -43,8 +48,10 @@ fn double() {
     // by 2) before it is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
-
-    println!("You can double x {} times before it is larger than 500", count);
+    println!(
+        "You can double x {} times before it is larger than 500",
+        count
+    );
 }
 
 fn count(arg: String) {
@@ -52,7 +59,6 @@ fn count(arg: String) {
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
